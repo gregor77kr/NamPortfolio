df01601f0b4090629322ceb50ba61591fbc74449
gregor77kr <7837167@naver.com>
2018-07-21 12:04:51 +0900

    member 테이블 칼럼 추가

--- blog.sql
+++ blog.sql
@@ -1,3 +1,6 @@
+/** sequence 초기화 **/
+drop sequence member_seq;
+
 CREATE SEQUENCE member_seq
 start with 1
 increment by 1
@@ -11,11 +14,22 @@ FROM dual;
 sequence int -> char 변환 확인 
 SELECT TO_CHAR(member_seq.nextval) FROM DUAL; **/
 
+/** 테이블 초기화 **/
+--begin
+--for c in (select table_name from user_tables) loop
+--execute immediate('drop table '||c.table_name||' cascade constraints');
+--end loop;
+--END;
+
+
+
 /** 테이블 생성 **/
 CREATE TABLE member(
 MEM_NO varchar2(20) primary key,
 USER_ID varchar2(20) not null unique check(length(user_id) between 8 AND 15),
 PASSWD varchar2(20) not null check(length(passwd) between 10 AND 20),
+USER_NAME varchar2(20) not null,
+BIRTH_DATE DATE not null,
 MOBILE_NUM varchar2(15) not null,
 ADDRESS varchar2(50) not null,
 EMAIL varchar2(20) not null,
@@ -52,13 +66,22 @@ commit;
 
 /** 샘플 데이터 INSERT **/
 
-INSERT INTO MEMBER VALUES (TO_CHAR(MEMBER_SEQ.NEXTVAL), 'greatch1', 'dkdldhsehdgml90', '010-3059-2222',
-'서울시 동대문구', '7837167@naver.com', sysdate);
+--INSERT INTO MEMBER VALUES (TO_CHAR(MEMBER_SEQ.NEXTVAL), 'greatch1', 'dkdldhsehdgml90', '010-3059-2222',
+--'서울시 동대문구', '7837167@naver.com', sysdate);
 
 select *
 from member;
-
+--
 /** foreign key 추가 **/
 alter table profile add constraint profile_mem_no_fk foreign key(mem_no)
 REFERENCES member(mem_no);
+
+ALTER TABLE board_master add constraint board_master_mem_no_fk foreign key(mem_no)
+REFERENCES member(mem_no);
+
+alter table friend add constraint friend_mem_no_fk foreign key(mem_no)
+REFERENCES member(mem_no);
+
+commit;
+
 
