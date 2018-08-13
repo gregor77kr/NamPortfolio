5d9a318c8c884652bfef8888ae94e87c0859b21f
78371 <78371@DESKTOP-6DRCI0V>
2018-08-14 01:45:33 +0900

    ajax write 기능 추가 필요

---
+++ blog/src/main/java/com/dailyblog/www/controller/ReplyController.java
@@ -0,0 +1,46 @@
+package com.dailyblog.www.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dailyblog.www.model.reply.dto.ReplyDto;
+import com.dailyblog.www.service.reply.ReplyService;
+
+@Controller
+@RequestMapping("/reply/*")
+public class ReplyController {
+	
+	private static final Logger log = LoggerFactory.getLogger(ReplyController.class);
+	
+	@Inject
+	ReplyService replySerivce;
+	
+	@RequestMapping("write.do")
+	public String write(HttpSession session, ReplyDto dto) {
+		
+		log.info("dto :" + dto);
+		replySerivce.replyWrite(session, dto);
+		
+		return "redirect:/board/main.do";
+	}
+	
+	@RequestMapping("readAll.do")
+	@ResponseBody
+	public List<ReplyDto> readAll(@RequestParam(value="up_no") String up_no) {
+		// 게시물 목록
+		log.info("up_no :" + up_no);
+		List<ReplyDto> list = replySerivce.readAll(up_no);
+
+		
+		return list;
+	}
+}
